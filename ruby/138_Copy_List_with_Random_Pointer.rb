2a7aede1150b1451b269ad0ca394c0bb7e735b2f
Jaime <jasmo2.com@gmail.com>
2021-01-07 11:06:00 -0500

    138 Copy List with random Pointer

---
+++ ruby/138_Copy_List_with_Random_Pointer.rb
@@ -0,0 +1,44 @@
+# Definition for Node.
+# class Node
+#     attr_accessor :val, :next, :random
+#     def initialize(val = 0)
+#         @val = val
+#		  @next = nil
+#		  @random = nil
+#     end
+# end
+
+# @param {Node} node
+# @return {Node}
+def copyRandomList(head)
+  # Modify original structure: Original->Copy->Original->Copy
+  # node.next.random = node.random.next
+  pointer = head
+  head_copy = nil
+
+  until pointer.nil?
+    next_pointer = pointer.next
+    copy = Node.new(pointer.val)
+    pointer.next = copy
+    copy.next = next_pointer
+    pointer = next_pointer
+  end
+  pointer = head
+
+  until pointer.nil?
+    pointer.next.random = pointer.random.next unless pointer.random.nil?
+    pointer = pointer.next.next
+  end
+  pointer = head
+
+  head_copy = pointer.next unless pointer.nil?
+
+  until pointer.nil?
+    copy = pointer.next
+    pointer.next = copy.next
+    pointer = pointer.next
+    copy.next = pointer.next unless pointer.nil?
+  end
+
+  head_copy
+end
